ad6e565bfa6ff6f16ac1863745b341b121ac6f08
Will Bartlett <will118@gmail.com>
2018-01-11 15:49:53 +0000

    Adds nested definitions and removes top level one.

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const fs = require('fs');
+const { join } = require('path');
 const Ajv = require('ajv');
-const schema = JSON.parse(fs.readFileSync('schema.json', 'utf8'));
-const sectionSchema = JSON.parse(fs.readFileSync('definitions/section.json', 'utf8'));
+
+const DEFINITION_DIR = 'definitions';
+
+const definitions = fs.readdirSync(DEFINITION_DIR).map(path => {
+  return JSON.parse(fs.readFileSync(join(DEFINITION_DIR, path), 'utf8'));
+});
 
 const ajv = new Ajv({
   schemaId: 'id',
-  schemas: [ schema, sectionSchema ]
+  schemas: definitions
 });
 
 ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
 
 const data = JSON.parse(fs.readFileSync('dictionary.json', 'utf8'));
 
-const valid = ajv.validate('base', data);
+const getOfType = type => data.sections.find(x => x.objectType === type);
+
+const section = getOfType('MPSection');
+
+const valid = ajv.validate('mp-section.json', section);
 
 if (!valid) {
   console.log(ajv.errors);
