37f1a7e1b330fba44892a6a3cc202b3d5deb82c2
Will Bartlett <will118@gmail.com>
2018-01-19 12:30:54 +0000

    Adds ajv-merge-patch.

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 const fs = require('fs');
 const { join } = require('path');
 const Ajv = require('ajv');
+const ajvMergeFeature = require('ajv-merge-patch/keywords/merge');
+
+const ajv = new Ajv();
+ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
+ajvMergeFeature(ajv);
 
 const DEFINITION_DIR = 'definitions';
 
-const definitions = fs.readdirSync(DEFINITION_DIR).map(path => {
-  return JSON.parse(fs.readFileSync(join(DEFINITION_DIR, path), 'utf8'));
+fs.readdirSync(DEFINITION_DIR).forEach(path => {
+  const schemaPath = join(DEFINITION_DIR, path);
+  const file = fs.readFileSync(schemaPath, 'utf8');
+  ajv.addSchema(JSON.parse(file));
 });
-
-const ajv = new Ajv({
-  logger: false,
-  schemaId: 'id',
-  schemas: definitions
-});
-
-ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
 
 const data = JSON.parse(fs.readFileSync('dictionary.json', 'utf8'));
 
-function testType(schema, type) {
-  const valid = ajv.validate(
-    schema,
-    data.sections.find(x => x.objectType === type)
-  );
+function testType(schema, type, object) {
+  const valid = ajv.validate(schema, object);
 
   if (!valid) {
     console.log(type + ': ✗');
@@ -32,7 +28,22 @@ function testType(schema, type) {
   }
 }
 
+const findType = (objects, type) => objects.find(x => x.objectType === type);
+
 [
-  ['mp-section.json', 'MPSection'],
-  ['mp-paragraph-element.json', 'MPParagraphElement'],
+  [
+    'mp-section.json',
+    'MPSection',
+    findType(data.sections, 'MPSection')
+  ],
+  [
+    'mp-paragraph-element.json',
+    'MPParagraphElement',
+    findType(data.sections, 'MPParagraphElement')
+  ],
+  [
+    'mp-paragraph-style.json',
+    'MPParagraphStyle',
+    findType(data.manuscript, 'MPParagraphStyle')
+  ],
 ].forEach(args => testType(...args));
