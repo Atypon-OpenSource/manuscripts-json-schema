009dcdb84a18d2c39b0c95468a6a20a03cd2219f
Will Bartlett <will118@gmail.com>
2018-04-05 14:48:18 +0100

    Exports validator function for use as module, adds __dirname.

--- getSchema.js
+++ getSchema.js
@@ -2,13 +2,13 @@ const { join } = require('path');
 const { readFileSync, readdirSync } = require('fs');
 
 function getSchema(name, directory) {
-  const schemaPath = join('definitions', directory || '', name);
+  const schemaPath = join(__dirname, 'definitions', directory || '', name);
   const file = readFileSync(schemaPath, 'utf8');
   return JSON.parse(file);
 }
 
 function getSchemas(directory) {
-  const schemasPath = join('definitions', directory);
+  const schemasPath = join(__dirname, 'definitions', directory);
   const files = readdirSync(schemasPath, 'utf8');
   return files.map(name => getSchema(name, directory));
 }
