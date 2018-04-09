cee80543f3b02556b6eeb69736d1a486f2b3f6b0
Will Bartlett <will118@gmail.com>
2018-04-09 15:31:25 +0100

    Further refactoring

--- src/exportDefinitions.ts
+++ src/exportDefinitions.ts
@@ -1,7 +1,14 @@
+import { join } from 'path';
 import { readdirSync, writeFileSync } from 'fs';
 import { compileFromFile } from 'json-schema-to-typescript';
 
-for (const file of readdirSync('definitions', 'utf8')) {
-  compileFromFile(`definitions/${file}`, { cwd: 'definitions' })
-    .then(ts => writeFileSync(`${file.replace('.json', '')}.d.ts`, ts))
+const SCHEMA_DIR = 'schemas';
+const DEFINITION_DIR = 'definitions';
+
+for (const file of readdirSync(SCHEMA_DIR, 'utf8')) {
+  compileFromFile(join(SCHEMA_DIR, file), { cwd: SCHEMA_DIR })
+    .then(ts => {
+      const path = join(DEFINITION_DIR, `${file.replace('.json', '')}.d.ts`);
+      writeFileSync(path, ts)
+    })
 }
--- src/exportSchemas.ts
+++ src/exportSchemas.ts
@@ -2,12 +2,7 @@ import { writeFileSync } from 'fs';
 import { join } from 'path';
 import { schemas } from './validate';
 
-const outDir = 'definitions';
-
-console.warn('Writing schemas to:', outDir);
-
 for (const schema of schemas) {
-  const path = join(__dirname, '..', outDir, schema.$id);
-  console.log('Writing schema to:', path);
+  const path = join(__dirname, '..', 'schemas', schema.$id);
   writeFileSync(path, JSON.stringify(schema, null, 2), 'utf8');
 }
