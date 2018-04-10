b769cabacba3727fc7d5fa55fe7d2118a6ebee89
Will Bartlett <will118@gmail.com>
2018-04-10 14:14:00 +0100

    Excludes some files that aren't used and can't be emitted anyway.

--- src/exportDefinitions.ts
+++ src/exportDefinitions.ts
@@ -11,11 +11,20 @@ const DIST_DIR = 'dist';
 const INDEX_D_TS = join(DIST_DIR, 'index.d.ts');
 const TYPES_D_TS = join(DIST_DIR, 'types.ts');
 
-Promise.all(readdirSync(SCHEMA_DIR, 'utf8').map(file => {
+const excludedFiles = new Set(['numbers.json', 'strings.json']);
+
+const schemas = readdirSync(SCHEMA_DIR, 'utf8').filter((file: string) => {
+  return !excludedFiles.has(file);
+});
+
+Promise.all(schemas.map(file => {
   return compileFromFile(join(SCHEMA_DIR, file), {
     cwd: SCHEMA_DIR,
     bannerComment: ''
-  }).then(definition => definition.replace(/^export\sinterface\sMp/, 'export interface '));
+  }).then(definition => {
+    // this is the best approach because the `objectType` has to match the mac.
+    return definition.replace(/^export\sinterface\sMp/, 'export interface ');
+  });
 })).then(definitions => {
   return writeFilePromise(TYPES_D_TS, definitions.join('\n'), 'utf8')
     .then(() => readFilePromise(INDEX_D_TS, 'utf8'))
