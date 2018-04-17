04ae998d9c35d8f025f81b23ebecf67ecc9620a7
Will Bartlett <will118@gmail.com>
2018-04-17 14:45:01 +0100

    Removes schemas dir and adds code to create it where needed.

--- src/exportSchemas.ts
+++ src/exportSchemas.ts
@@ -1,8 +1,14 @@
-import { writeFileSync } from 'fs';
+import { writeFileSync, existsSync, mkdirSync } from 'fs';
 import { join } from 'path';
 import { schemas } from './schemas';
 
+const schemaDir = join(__dirname, '..', 'schemas');
+
+if (!existsSync(schemaDir)){
+  mkdirSync(schemaDir);
+}
+
 for (const schema of schemas) {
-  const path = join(__dirname, '..', 'schemas', schema.$id);
+  const path = join(schemaDir, schema.$id);
   writeFileSync(path, JSON.stringify(schema, null, 2), 'utf8');
 }
