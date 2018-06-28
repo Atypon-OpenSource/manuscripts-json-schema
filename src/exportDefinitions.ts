8e4e646dc69988a5f28b303e8a9b16cf2ea1c985
Will Bartlett <will118@gmail.com>
2018-06-28 16:07:45 +0100

    Exports typescript definitons for abstract/base classes

--- src/exportDefinitions.ts
+++ src/exportDefinitions.ts
@@ -1,38 +1,66 @@
+import mash from './mash';
 import { join } from 'path';
 import { promisify } from 'util';
 import { readdirSync, writeFile, readFile } from 'fs';
-import { compileFromFile } from 'json-schema-to-typescript';
+import { compile, compileFromFile } from 'json-schema-to-typescript';
+import { getSchemas } from './getSchema';
 
 const writeFilePromise = promisify(writeFile);
 const readFilePromise = promisify(readFile);
 
-const SCHEMA_DIR = 'schemas';
-const DIST_DIR = 'dist';
-const INDEX_D_TS = join(DIST_DIR, 'index.d.ts');
-const TYPES_D_TS = join(DIST_DIR, 'types.ts');
+async function getCompiledSchemas() {
+  const schemaDir = 'schemas';
+  const excludedFiles = new Set(['numbers.json', 'strings.json']);
 
-const excludedFiles = new Set(['numbers.json', 'strings.json']);
+  const schemaDefinitions = await Promise.all(
+    readdirSync(schemaDir, 'utf8')
+      .filter(file => !excludedFiles.has(file))
+      .map(file => {
+        return compileFromFile(join(schemaDir, file), {
+          cwd: schemaDir,
+          bannerComment: ''
+        })
+      })
+  );
 
-const schemas = readdirSync(SCHEMA_DIR, 'utf8').filter((file: string) => {
-  return !excludedFiles.has(file);
-});
+  return schemaDefinitions;
+}
 
-Promise.all(schemas.map(file => {
-  return compileFromFile(join(SCHEMA_DIR, file), {
-    cwd: SCHEMA_DIR,
-    bannerComment: ''
-  }).then(definition => {
-    // this is the best approach because the `objectType` has to match the mac.
-    return definition.replace(/^export\sinterface\sMp/, 'export interface ');
-  });
-})).then(definitions => {
-  return writeFilePromise(TYPES_D_TS, definitions.join('\n'), 'utf8')
-    .then(() => readFilePromise(INDEX_D_TS, 'utf8'))
-    .then((indexContents: string) => {
-      const contents = [
-        indexContents,
-        "export * from './types';"
-      ].join('\n');
-      return writeFilePromise(INDEX_D_TS, contents, 'utf8')
-    });
-});
+async function getAbstractSchemas() {
+  return Promise.all(
+    getSchemas('abstract')
+      .map(mash)
+      .map(schema => {
+        return compile(schema, schema.$id, {
+          cwd: 'schemas',
+          bannerComment: ''
+        })
+      })
+  );
+}
+
+(async function() {
+  const DIST_DIR = 'dist';
+
+  const INDEX_D_TS = join(DIST_DIR, 'index.d.ts');
+  const existingIndexContents = await readFilePromise(INDEX_D_TS, 'utf8')
+  const indexContents = [
+    existingIndexContents,
+    "export * from './types';"
+  ].join('\n');
+
+  // append an export to these types in the index.d.ts file
+  await writeFilePromise(INDEX_D_TS, indexContents, 'utf8')
+
+  const schemas = await getCompiledSchemas();
+  const abstracts = await getAbstractSchemas();
+
+  const contents = await Promise.all(
+    schemas.concat(abstracts).map(definition => {
+      // this is the best approach because the `objectType` has to match the mac.
+      return definition.replace(/^export\sinterface\sMp/, 'export interface ');
+    })
+  );
+
+  await writeFilePromise(join(DIST_DIR, 'types.ts'), contents.join('\n'), 'utf8')
+})()
