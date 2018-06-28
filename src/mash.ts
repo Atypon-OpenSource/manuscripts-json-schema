997fbe87c5f1146f070ba8d510ad51e0210556a3
Will Bartlett <will118@gmail.com>
2018-06-28 09:46:16 +0000

    Allow additional properties on BibliographicName

--- src/mash.ts
+++ src/mash.ts
@@ -2,13 +2,14 @@ const merge = require('deepmerge')
 import { getSchema } from './getSchema';
 
 export default function mash(obj: any) {
+  const { additionalProperties } = obj;
   if (obj.$mash) {
     const { sources } = obj.$mash;
     const merged = sources.reduce((acc: any, source: any) => {
       const schemaId = source.$ref.slice(0, -1);
       const schema = getSchema(schemaId);
       const mashed = mash(schema);
-      const objectType = acc.properties.objectType;
+      const { objectType } = acc.properties;
       const merged = merge(mashed, acc);
       // We don't want array merging for this enum.
       if (objectType) {
@@ -18,6 +19,9 @@ export default function mash(obj: any) {
     }, obj.$mash.with);
     merged.$id = obj.$id;
     merged.title = obj.title;
+    if (additionalProperties) {
+      merged.additionalProperties = additionalProperties;
+    }
     return merged;
   }
   return obj;
