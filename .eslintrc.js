53bdd6735a7a4ff6a667b9af67d1fc2fd607d4e7
m-bartenev <mbartenev@atypon.com>
2026-05-14 14:53:18 +0200

    versions updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'off',
     'react/no-unescaped-entities': 'off',
     'jsx-a11y/no-autofocus': 'off',
+    'import/no-unresolved': ['error', { ignore: ['^uuid$'] }],
   },
 }
